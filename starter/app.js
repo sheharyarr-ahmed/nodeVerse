ad221687adc36a4649f2fb56c944075769d0065c
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-11 04:43:53 +0500

    feat: add tours API endpoints

--- starter/app.js
+++ starter/app.js
@@ -1,16 +1,54 @@
+const fs = require('fs');
+
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the sever side!', app: 'natours' });
+  res.status(200).json({
+    message: 'Hello from the server side!',
+    app: 'natours',
+    toursUrl: '/api/v1/tours',
+  });
 });
 
-app.post('/', (res, res) => {
-  res.send('you canpost to thsi endpoint...');
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint.');
 });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'),
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+});
+
 const port = 3000;
 
 app.listen(port, () => {
