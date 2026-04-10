887538bfa08775a1d8450897394f09973de5f4a5
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-11 04:54:42 +0500

    feat: add endpoint for fetching tour by id

--- starter/app.js
+++ starter/app.js
@@ -29,6 +29,25 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
 
 app.post('/api/v1/tours', (req, res) => {
   console.log(req.body);
