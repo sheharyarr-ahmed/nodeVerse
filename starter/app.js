ba96db9d378e2fe6d8d79fcb0fe22552877c7053
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-11 08:15:06 +0500

    feat: add dotenv environment configuration

--- starter/app.js
+++ starter/app.js
@@ -5,7 +5,10 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
--- starter/server.js
+++ starter/server.js
@@ -1,6 +1,13 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: `${__dirname}/config.env` });
+
 const app = require('./app');
 
-const port = 3000;
+console.log('Environment:', process.env.NODE_ENV);
+console.log('Port:', process.env.PORT);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
