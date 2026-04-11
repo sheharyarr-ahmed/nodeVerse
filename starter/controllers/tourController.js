804e841d392dd9ecc1778d533fbf8bbcfe93aebf
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-11 23:38:54 +0500

    feat: add advanced tour query filtering

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -1,5 +1,45 @@
 const Tour = require('../models/tourModel');
 
+const allowedOperators = ['gte', 'gt', 'lte', 'lt'];
+
+const buildToursQuery = (queryString) => {
+  const queryObj = { ...queryString };
+  const excludedFields = ['page', 'sort', 'limit', 'fields'];
+  excludedFields.forEach((field) => delete queryObj[field]);
+
+  let query = Tour.find();
+
+  Object.entries(queryObj).forEach(([field, value]) => {
+    if (typeof value === 'object' && value !== null) {
+      Object.entries(value).forEach(([operator, operatorValue]) => {
+        if (!allowedOperators.includes(operator)) return;
+
+        query = query.where(field)[operator](operatorValue);
+      });
+    } else {
+      query = query.where(field).equals(value);
+    }
+  });
+
+  if (queryString.sort) {
+    query = query.sort(queryString.sort.split(',').join(' '));
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  if (queryString.fields) {
+    query = query.select(queryString.fields.split(',').join(' '));
+  } else {
+    query = query.select('-__v');
+  }
+
+  const page = queryString.page * 1 || 1;
+  const limit = queryString.limit * 1 || 100;
+  const skip = (page - 1) * limit;
+
+  return query.skip(skip).limit(limit);
+};
+
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
@@ -13,7 +53,7 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const tours = await buildToursQuery(req.query);
 
     res.status(200).json({
       status: 'success',
