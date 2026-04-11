fe32cf4ea2237167873a34ba8c8748b24a0af8df
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-11 21:30:05 +0500

    feat: expand tour schema fields

--- starter/models/tourModel.js
+++ starter/models/tourModel.js
@@ -7,14 +7,40 @@ const tourSchema = new mongoose.Schema({
     unique: true,
   },
 
-  rating: {
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
 
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
+  },
+
+  priceDiscount: {
+    summary: {
+      type: String,
+      trim: true,
+    },
   },
 });
 
