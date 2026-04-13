607861b6d9f2ceb2609dc1b68dab59d62efda446
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-13 06:22:38 +0500

    feat: add user model with validation

---
+++ starter/models/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: function (val) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val);
+      },
+      message: 'Please provide a valid email',
+    },
+  },
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'A password must have at least 8 characters'],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
