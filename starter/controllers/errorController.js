5075a5551d2fe217bed5b6e66b36bb142e190a76
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-13 05:27:31 +0500

    feat: add global operational error handling

---
+++ starter/controllers/errorController.js
@@ -0,0 +1,35 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  console.error('ERROR:', err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Something went very wrong!',
+  });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else {
+    sendErrorProd(err, res);
+  }
+};
