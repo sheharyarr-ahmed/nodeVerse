26f62a24be378fda2bfe0d6efb9a0b16025e6844
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-13 08:34:37 +0500

    feat(auth): issue JWT on user signup

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -1,5 +1,12 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,8 +17,12 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  const token = signToken(newUser._id);
+  newUser.password = undefined;
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
--- starter/models/userModel.js
+++ starter/models/userModel.js
@@ -42,12 +42,11 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
-  next();
 });
 
 const User = mongoose.model('User', userSchema);
