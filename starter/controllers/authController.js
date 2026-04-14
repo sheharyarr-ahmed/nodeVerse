857324154a9a8d77828c73e12fc88d4f390fd0aa
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-14 18:14:57 +0500

    feat: add user login and listing endpoints

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 
+const AppError = require('../utils/appError');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -25,6 +26,31 @@ exports.signup = catchAsync(async (req, res, next) => {
     token,
     data: {
       user: newUser,
+    },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  // check if user exists and password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  const token = signToken(user._id);
+  user.password = undefined;
+
+  res.status(200).json({
+    status: 'success',
+    token,
+    data: {
+      user,
     },
   });
 });
