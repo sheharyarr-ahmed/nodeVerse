a9acacef5017a6bbcaceb209a9beb3f29af73201
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-15 13:23:20 +0500

    feat: add API security middleware and token response helper

--- starter/app.js
+++ starter/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,10 +9,21 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+app.use(helmet());
+
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
