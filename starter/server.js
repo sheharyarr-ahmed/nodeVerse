8dcdcbf9e45904a72e3845b8970f13d93c08a2e6
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-04-19 11:18:55 +0500

    heroku configurations

--- starter/server.js
+++ starter/server.js
@@ -48,3 +48,11 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully.');
+
+  server.close(() => {
+    console.log('Process terminated.');
+  });
+});
